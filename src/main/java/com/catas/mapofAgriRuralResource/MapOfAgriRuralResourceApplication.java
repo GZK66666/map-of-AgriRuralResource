6e35bd796fde4dabf932a0fb59a636588b6023da
GZK66666 <572223078@qq.com>
2024-02-27 09:43:58 +0800

    init project structure

---
+++ src/main/java/com/catas/mapofAgriRuralResource/MapOfAgriRuralResourceApplication.java
@@ -0,0 +1,13 @@
+package com.catas.mapofAgriRuralResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapOfAgriRuralResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapOfAgriRuralResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catas/mapofAgriRuralResource/MapOfAgriRuralResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catas.mapofAgriRuralResource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapOfAgriRuralResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
